53814779163781ca48404523b985ed15fa244235
Penas10S-ay2627 <penas.kr@obmontessori.edu.ph>
2026-08-23 18:37:29 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+# Data Types in Python
+from pyscript import display, document
+
+# String
+name = 'Peñas, Katharine'
+# Integer
+age = 15
+# Float
+height1ncm = 152.4
+# List
+countries2_visit = ['Canada', 'United Kingdom','Norway']
+# Boolean
+student_type = False
+# Dictionary
+dictionary = {'color':'Blue', 'car_brand':'Toyota', 'shoe_size':'8', 'best_friend':'Julianne'}
+# Set
+favorite_fruit = set(['apple', 'grape', 'banana', 'blueberry', 'watermelon'])
+# Tuple
+daysofthe_week = ('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday')
+
+display(type(name), target='result')
+display(type(age), target='result')
+display(type(height1ncm), target='result')
+display(type(countries2_visit), target='result')
+display(type(student_type), target='result')
+display(type(dictionary), target='result')
+display(type(favorite_fruit), target='result')
+display(type(daysofthe_week), target='result')
